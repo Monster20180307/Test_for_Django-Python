0d1e9a4108a727eed52d16a1c2955233212ba1e1
Monster20180307 <619580335@qq.com>
2018-03-07 22:23:33 +0800

    yewei 20180307 for Django add

--- DjangoCase/DjangoCase/settings.py
+++ DjangoCase/DjangoCase/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+'''添加应用'''
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sign',
 ]
 
 MIDDLEWARE = [
